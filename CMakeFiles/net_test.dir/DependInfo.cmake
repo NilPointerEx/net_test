6b7631aa7e54b7983a292004d7374f4d7a5354c4
zodiac <zsbtiger@gmail.com>
2014-11-05 09:44:33 +0800

    add files

---
+++ CMakeFiles/net_test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zodiac/git/net_test/test_echo.cpp" "/home/zodiac/git/net_test/CMakeFiles/net_test.dir/test_echo.cpp.o"
+  "/home/zodiac/git/net_test/test_main.cpp" "/home/zodiac/git/net_test/CMakeFiles/net_test.dir/test_main.cpp.o"
+  "/home/zodiac/git/net_test/test_tcp.cpp" "/home/zodiac/git/net_test/CMakeFiles/net_test.dir/test_tcp.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/net_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/net_test.dir/test_tcp.cpp.o"
+  "CMakeFiles/net_test.dir/test_echo.cpp.o"
+  "CMakeFiles/net_test.dir/test_main.cpp.o"
+  "net_test.pdb"
+  "net_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/net_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
